d05fcd15e4c933a9217a672687cdd5b33a576e68
prydsy <pramanayuda772@gmail.com>
2022-03-17 00:19:36 +0700

    cors

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const cors = require('cors')
 
-
 const app = express()
 
+const corsOptions = {
+    origin: '*',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({
+    extended: true
+}))
 
 const db = require('./app/models/')
 db.mongoose
@@ -30,5 +38,5 @@ require('./app/routes/task.routes')(app)
 
 const PORT = 3000
 app.listen(process.env.PORT || PORT, () => {
- console.log(`Server is running on port ${PORT}`)   
+    console.log(`Server is running on port ${PORT}`)
 })
