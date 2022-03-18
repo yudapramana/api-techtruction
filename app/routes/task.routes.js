a4c5c48a424bd2f4a4b38f0eb718fa7ca38000ce
prydsy <pramanayuda772@gmail.com>
2022-03-18 23:36:29 +0700

    push

--- app/routes/task.routes.js
+++ app/routes/task.routes.js
@@ -5,11 +5,11 @@ module.exports = (app) => {
     const tasks = require('../controllers/task.controller');
     const router = require('express').Router();
 
-    router.get('/', verifyUser, tasks.findAll);
-    router.get('/:id', verifyUser, tasks.findOne);
-    router.post('/', verifyUser, tasks.create);
-    router.put('/:id', verifyUser, tasks.update);
-    router.delete('/:id', verifyUser, tasks.delete);
+    router.get('/', tasks.findAll);
+    router.get('/:id', tasks.findOne);
+    router.post('/', tasks.create);
+    router.put('/:id', tasks.update);
+    router.delete('/:id', tasks.delete);
 
     app.use('/api/tasks', router);
 
